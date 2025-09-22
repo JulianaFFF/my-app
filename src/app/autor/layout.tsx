aefcb8fc4380143ee2e77991dba16fc089e0c69a
JulianaFFF <lj.ferreira@uniandes.edu.co>
2025-09-21 20:09:38 -0500

    footer en la lista

--- src/app/autor/layout.tsx
+++ src/app/autor/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function AutorLayout({
   children,
@@ -19,6 +20,7 @@ export default function AutorLayout({
       <main className="flex-grow">
         {children}{" "}
         {/* The current page will be rendered here (e.g., students/page.tsx). */}
+      <Footer/>
       </main>
     </div>
   );
